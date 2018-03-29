4be050c5cd83c46d11c7d7154a573cc6bc912d4d
michaelkunc <mjkunc@gmail.com>
2018-03-29 08:22:17 -0700

    added params to Occupation Controller

--- src/main/java/com/fake_papers/OccupationController.java
+++ src/main/java/com/fake_papers/OccupationController.java
@@ -1,13 +1,24 @@
 package com.fake_papers;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OccupationController {
 
-    @RequestMapping("/occupation")
-    public Occupation occupation(){
-        return new Occupation();
+    @RequestMapping("/occupations")
+    public List<Occupation> occupation(@RequestParam(value="qty", defaultValue="1") String qty){
+        int quantity = Integer.parseInt(qty);
+        List<Occupation> results = new LinkedList<Occupation>();
+        for (int i = 0; i < quantity; i++){
+            Occupation occ = new Occupation();
+            results.add(occ);
+        }
+        return results;
     }
+
 }
