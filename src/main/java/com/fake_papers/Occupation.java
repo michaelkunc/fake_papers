87d113426d14d0d69db82951110e4c4d987591a3
michaelkunc <mjkunc@gmail.com>
2018-03-30 13:29:11 -0700

    add occupations

--- src/main/java/com/fake_papers/Occupation.java
+++ src/main/java/com/fake_papers/Occupation.java
@@ -1,11 +1,24 @@
 package com.fake_papers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Occupation {
     
     private final String occupation;
+    private final Random random = new Random();
 
     public Occupation(){
-        this.occupation = "Emperor";
+        List<String> occupations = new ArrayList<String>();
+        occupations.add("Emperor");
+        occupations.add("Witcher");
+        occupations.add("Soldier");
+        occupations.add("Sorceress");
+        occupations.add("Sorcerer");
+
+        int index = random.nextInt(occupations.size());
+        this.occupation = occupations.get(index);
     }
 
     public String getOccupation(){
