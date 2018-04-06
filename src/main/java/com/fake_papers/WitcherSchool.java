e6b7a266e1d9a6013c9ce17fa9d588d24d3f989c
michaelkunc <mjkunc@gmail.com>
2018-04-06 07:53:52 -0700

    updates to WitcherSchool list and witcherschools

--- src/main/java/com/fake_papers/WitcherSchool.java
+++ src/main/java/com/fake_papers/WitcherSchool.java
@@ -11,15 +11,17 @@ public class WitcherSchool {
     private final Random random = new Random();
 
     public WitcherSchool(){
-        List <String> schools = new ArrayList<String>();
-        schools.add("The School of The Cat");
-        schools.add("The School of The Wolf");
-        schools.add("The School of The Bear");
-        schools.add("The School of The Viper");
-        schools.add("The School of The Manticore");
+        // List <String> schools = new ArrayList<String>();
+        // schools.add("The School of The Cat");
+        // schools.add("The School of The Wolf");
+        // schools.add("The School of The Bear");
+        // schools.add("The School of The Viper");
+        // schools.add("The School of The Manticore");
 
-        int index = random.nextInt(schools.size());
-        this.witcherschool = schools.get(index);
+        // int index = random.nextInt(schools.size());
+        // this.witcherschool = schools.get(index);
+        int index = random.nextInt(WitcherSchoolsList.values().length);
+        this.witcherschool = WitcherSchoolsList.values()[index].toString();
 
     }
 
--- src/main/java/com/fake_papers/WitcherSchoolsList.java
+++ src/main/java/com/fake_papers/WitcherSchoolsList.java
@@ -1,5 +1,17 @@
 package com.fake_papers;
 
 public enum WitcherSchoolsList {
-    CAT, WOLF, BEAR, VIPER, MANTICORE
+    CAT ("The School of The Cat"), 
+    WOLF ("The School of The Wolf"), 
+    BEAR ("The School of The Bear"), 
+    VIPER ("The School of The Viper"), 
+    MANTICORE ("The School of The Manticore");
+
+    private String school; 
+
+    WitcherSchoolsList(String school){
+        this.school = school;
+    }
+
+
 }
